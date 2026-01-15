a3013c660cf721c6a91ab2b8bf434122de219830
sjackss <148665160+sjackss@users.noreply.github.com>
2026-01-15 12:40:57 +1000

    Add copyright notice to api-cleanup-messages.js
    Signed-off-by: sjackss <148665160+sjackss@users.noreply.github.com>

--- oodles-app/api-cleanup-messages.js
+++ oodles-app/api-cleanup-messages.js
@@ -1,3 +1,12 @@
+/**
+ * Copyright (c) 2025 Oodles & Doodles
+ * All rights reserved.
+ * Owner: sjackss
+ * 
+ * This file is part of Oodles & Doodles dating application.
+ * Unauthorized copying, distribution, or use is strictly prohibited.
+ */
+
 // netlify/functions/api-cleanup-messages.js
 // Scheduled function to delete messages older than 14 days
 import { sql } from './db.js';
